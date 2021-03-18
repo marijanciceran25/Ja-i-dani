5a747b4a9e61eec57c08fd32910e42f1d319ba98
Danijel Dupor <danijeldupor.dd@gmail.com>
2021-03-18 20:37:34 +0100

    signup2

--- src/views/Sign_in.vue
+++ src/views/Sign_in.vue
@@ -1,77 +1,81 @@
 <template>
-    <article>
-        <div class="container">
-            <div class="overlay-container">
-                <div class="overlay">
-                    <div class="overlay-left">
-                        <h2>Dobrodošli natrag</h2>
-                        <p>Molimo upišite osobne podatke</p>
-                        <button>Prijava</button>
-                    </div>
-                    <div class="overlay-right">
-                        <h2>Pozdrav</h2>
-                        <p>Molimo unesite vaše osobne detalje</p>
-                        <button>Registracija</button>
-                    </div>
+   
+        <div class="form_group">
+            <div class="row">
+                <div class="col">
+            <b-card>
+            <h3>Sign Up</h3>
+
+            <div class="form-group">
+                <label>Email</label>
+                <input 
+                    type="email"
+                    class="form-control"
+                    id="exampleInputEmail"
+                    aria-describedby="emailHelp" 
+                    placeholder="Email"/>
+            </div>
+            <br>
+           
+            <div class="form-group">
+                <label for="exampleInputPassword1">Lozinka</label>
+                <input type="password" class="form-control" placeholder="Lozinka" />
+            </div>
+            <br>
+
+            <div class="form-group2">
+                <label for="exampleInputPassword2">Ponovite lozinku</label>
+                <input type="password" class="form-control" placeholder="Ponovite lozinku"/>
+            </div>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            
+            
+
+            <button class="btn btn-primary btn-block">Sign Up</button>
+            </b-card>
                 </div>
             </div>
         </div>
-    </article>
+    
 </template>
 
 <script>
 export default {
-    
+    name: 'Sign_in'
 }
 </script>
 
-<style lang="scss" scoped>
-.container {
-    position: relative;
-    width: 768px;
-    height: 480px;
+
+<style>
+.form_group {
+    padding-left: 30%;
+    padding-right: 30%;
+    border: none;
+    background-color:rgb(63, 60, 60);
+    height: 600px;
+}
+
+div.card-body {
+    background-color: #044ca4;
+    color: aliceblue;
+    height: 600px;
     border-radius: 10px;
-    overflow: hidden;
-    box-shadow: 0 15px 30px rgba(0, 0, 0, .2),
-                0 15px 30px rgba(0, 0, 0, .2);
-    background: linear-gradient(to bottom, #efefef, #ccc);
-
-    .overlay-container {
-        position: absolute;
-        top: 0;
-        left: 50%;
-        width: 50%;
-        height: 100%;
-        overflow: hidden;
-        transition: transform .5s ease-in-out;
-        z-index: 100;
-    }
-
-    .overlay{
-        position: relative;
-        left: -100%;
-        height: 100%;
-        width: 200%;
-        background: rgb(27,27,147);
-        background: linear-gradient(94deg, rgba(27,27,147,1) 82%, rgba(32,27,101,1) 100%, rgba(19,154,181,1) 100%);
-        transform: translateX(0);
-        transition: transform .5s ease-in-out;
-    }
-
-    @mixin overlays($property) {
-        position: absolute;
-        top: 0;
-        display: flex;
-        align-items: center;
-        justify-content: space-around;
-        flex-direction: column;
-        padding: 70px 40px;
-        width: calc(50% - 80px);
-        height: calc(100% - 140px);
-        text-align: center;
-        transform: translateX($property);
-        transition: transform .5s ease-in-out;
-    }            
+    
+  
+    
+}
+.form_group2 {
+    padding-bottom: 20%;
+}
+div.card {
+    border-radius: 10px;
 }
 
 </style>
