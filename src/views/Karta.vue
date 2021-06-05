344b37a52489a717dc73858e55d188f962e5ac9a
dkodrin <75334503+dkodrin@users.noreply.github.com>
2021-06-05 16:37:29 +0200

    Karta implementirana

--- src/views/Karta.vue
+++ src/views/Karta.vue
@@ -1,37 +1,26 @@
 <template>
 <div class="Mapa">
-    <Navbar></Navbar>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <h1>Mapa stranica</h1>
-    <br>
-    <br>
-    <center>
-    <b-row class="proba">
-        <b-col>
-        <b-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi reiciendis fuga error. Excepturi mollitia aspernatur, aliquid aut alias voluptate reprehenderit?</b-text>
-        </b-col>
-        </b-row>
-    </center>    
+    <Navbar></Navbar>   
+    <iframe src="https://snazzymaps.com/embed/317824" width="100%" height="1000px" style="border:none;"></iframe>
+    <Footer></Footer>
 </div>   
 </template>
 
 <script>
 import Navbar from '@/components/Navbar.vue';
+import Footer from '@/components/Footer.vue';
 
 export default {
     name: "Mapa",
     components: {
-        Navbar
+        Navbar,
+        Footer
     },
 };
 </script>
 <style>
 .Mapa {
-    background-color: turquoise;
+    background-color: black;
 }
 .proba {
     color: black;
